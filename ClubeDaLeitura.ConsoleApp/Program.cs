7a730106f3332648ace0002b0c9a249ea96b920a
Thiago Culau <thiago.culau1@outlook.com>
2024-05-07 16:30:48 -0300

    tentativa de erro de sincronização

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -2,18 +2,18 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            //aojsdhasiudhaisjdhiasjh
-            Console.WriteLine("Hello, World!");
+        //static void Main(string[] args)
+        // {
+        //aojsdhasiudhaisjdhiasjh
+        //Console.WriteLine("Hello, World!");
 
 
-            //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
-            //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
-            //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
-            //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
-            //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
-            //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
-        }
+        //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
+        //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
+        //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
+        //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
+        //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
+        //tersdaj;s.kldnasojdhasjikhdajhasuihdaisjdhaisojdhiasjdhaisjdhaijdhaijdshduijh
+        //}
     }
 }

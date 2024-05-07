609b475fbbecdad79e3114dbcb9ae4a116bff52f
Thiago Culau <thiago.culau1@outlook.com>
2024-05-07 17:08:03 -0300

    Classes geradas

---
+++ ClubeDaLeitura.ConsoleApp/Emprestimo.cs
@@ -0,0 +1,45 @@
+﻿namespace ClubeDaLeitura.ConsoleApp
+{
+    public class Emprestimo
+    {
+        public int Concluido
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Data
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int DataDevolucao
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Amigo Amigo
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Revista Revista
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ ClubeDaLeitura.ConsoleApp/Revista.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClubeDaLeitura.ConsoleApp
+{
+    public class Revista
+    {
+        public int Ano
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int NumeroEdicao
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Titulo
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public Caixa Caixa
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}

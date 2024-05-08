900142c537f96dfa7c47e87f6f27e17abf56d9c4
Thiago Culau <thiago.culau1@outlook.com>
2024-05-08 16:54:24 -0300

    Cabeçalho editado

--- ClubeDaLeitura.ConsoleApp/Compartilhado/TelaBase.cs
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/TelaBase.cs
@@ -9,9 +9,7 @@
         {
             Console.Clear();
 
-            Console.WriteLine("----------------------------------------");
-            Console.WriteLine($"        Gestão de {tipoEntidade}s        ");
-            Console.WriteLine("----------------------------------------");
+            Cabecalho();
 
             Console.WriteLine();
 
@@ -163,5 +161,97 @@
         }
 
         protected abstract EntidadeBase ObterRegistro();
+
+        #region Apenas Mensagens
+        public void Cabecalho()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Clear();
+            Console.WriteLine("########################################################################################################################");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                                           Academia do Programador 2024                                           ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                                                   Marea Turbo                                                    ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                                                Clube do Leitura                                                  ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("########################################################################################################################\n");
+            Console.ResetColor();
+        }
+        public void Erro()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+            Console.WriteLine("\n\n\n########################################################################################################################");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                                                      ATENÇÃO                                                     ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                               Comando inválido. Por favor digite um comando válido.                              ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                                      Precione qualquer tecla para continuar.                                     ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("########################################################################################################################");
+            Console.ReadKey();
+            Cabecalho();
+        }
+        public void ErroSemDadosCadastradosFuncionario()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+            Console.WriteLine("\n\n\n########################################################################################################################");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                                                      ATENÇÃO                                                     ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                             Nenhum cadastrado encontrado, direcionando para o cadastro                           ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                                      Precione qualquer tecla para continuar.                                     ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("########################################################################################################################");
+            Console.ReadKey();
+        }
+        public void DataInvalida()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+            Console.WriteLine("\n\n\n########################################################################################################################");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                                                      ATENÇÃO                                                     ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                      Data Inválida. Por favor digite um dia igual ou inferior ao dia atual.                      ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("###                                      Precione qualquer tecla para continuar.                                     ###");
+            Console.WriteLine("###                                                                                                                  ###");
+            Console.WriteLine("########################################################################################################################");
+            Console.ReadKey();
+            Cabecalho();
+        }
+        public void CadastroComSucesso()
+        {
+            Cabecalho();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Cadastro efetuado com sucesso!\n\nPrecione qualquer tecla para continuar.");
+            Console.ResetColor();
+        }
+        public void AlteracaoEfetuadaComSucesso()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\tAlteração efetuada com sucesso. Precione qualquer tecla para continuar");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+        #endregion
+
+        #region Mensagens com Opções
+
+        #endregion
+
+        #region Mensagens com Funções
+
+
+        #endregion
+
+
+
     }
 }
+

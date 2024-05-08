57a272a610d884b08058d4cc7027cc80410ea6b6
Thiago Culau <thiago.culau1@outlook.com>
2024-05-08 14:28:50 -0300

    Adicionado classe mensagens e uma filtrada em classes recicladas

--- ClubeDaLeitura.ConsoleApp/ModuloAmigo/Amigo.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloAmigo/Amigo.cs
@@ -4,20 +4,13 @@
     {
         public int Endereco { get; set; }
 
-
         public int Nome { get; set; }
-
 
         public int NomeResponsavel { get; set; }
 
         public int Telefone { get; set; }
 
-        public ClubeDaLeitura.ConsoleApp.ModuloMulta.Multa[] HistoricoDeMulta
-        {
-            get => default;
-            set
-            {
-            }
-        }
+        public ClubeDaLeitura.ConsoleApp.ModuloMulta.Multa[] HistoricoDeMulta { get; set; }
+
     }
 }

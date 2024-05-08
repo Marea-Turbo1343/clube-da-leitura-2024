d292c09696a2a557516fb1905d352fd9f229e323
Thiago Culau <thiago.culau1@outlook.com>
2024-05-08 15:09:18 -0300

    Extruturação da tela inicial reciclada

--- ClubeDaLeitura.ConsoleApp/Compartilhado/TelaPrincipal.cs
+++ ClubeDaLeitura.ConsoleApp/Compartilhado/TelaPrincipal.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ClubeDaLeitura.ConsoleApp.Compartilhado
+﻿namespace ClubeDaLeitura.ConsoleApp.Compartilhado
 {
-    internal class TelaPrincipal
+    internal static class TelaPrincipal
     {
+        public static char ApresentarMenuPrincipal()
+        {
+            Console.Clear();
+
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("|       Controle de Medicamentos       |");
+            Console.WriteLine("----------------------------------------");
+
+            Console.WriteLine();
+
+            Console.WriteLine("1 - Cadastro de Pacientes");
+            Console.WriteLine("2 - Cadastro de Medicamentos");
+            Console.WriteLine("3 - Cadastro de Requisições de Saída");
+
+            Console.WriteLine("S - Sair");
+
+            Console.WriteLine();
+
+            Console.Write("Escolha uma das opções: ");
+
+            char opcaoEscolhida = Console.ReadLine()[0];
+
+            return opcaoEscolhida;
+        }
     }
 }
--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -2,11 +2,43 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            //LerDecimalTelaEqueleto engine = new TelaEqueleto();
 
-            Console.WriteLine("Hello, World!");
+            int posicaoativafuncionario = 0;
 
+            engine.ChecagemPreLogin();
+
+            posicaoativafuncionario = engine.login.Login();
+
+            while (true)
+            {
+                int opcaomenu = engine.MenuPrincipal(posicaoativafuncionario);
+                switch (opcaomenu)
+                {
+                    case 1:
+                        engine.NovoCadastro();
+                        break;
+
+                    case 2:
+                        engine.MenuProntuario();
+                        break;
+
+                    case 3:
+                        engine.medicamento.SolicitarRemedio();
+                        break;
+
+                    case 4:
+                        engine.medicamento.VerficicarEstoque();
+                        break;
+
+                    case 5:
+                        Main();
+                        break;
+                }
+            }
         }
     }
+}
 }

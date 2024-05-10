e79ed2d7f67ca5b3cfd1fa8d2f4a6184405274c1
Allan <Allan@DESKTOP-RA9V1AJ>
2024-05-10 14:24:21 -0300

    Alterações

--- ClubeDaLeitura.ConsoleApp/ModuloAmigo/RepositorioAmigo.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloAmigo/RepositorioAmigo.cs
@@ -4,16 +4,14 @@ namespace ClubeDaLeitura.ConsoleApp.ModuloAmigo
 {
     internal class RepositorioAmigo : RepositorioBase
     {
-        private List<Amigo> amigos = new List<Amigo>();
-
         public void Atualizar(Amigo amigoAtualizado)
         {
-            for (int i = 0; i < amigos.Count; i++)
+            for (int i = 0; i < registros.Count; i++)
             {
-                Amigo amigoExistente = amigos[i];
+                Amigo amigoExistente = (Amigo)registros[i];
                 if (amigoExistente.Id == amigoAtualizado.Id)
                 {
-                    amigos[i] = amigoAtualizado;
+                    registros[i] = amigoAtualizado;
                     break;
                 }
             }
